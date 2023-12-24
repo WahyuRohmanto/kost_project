3e9a5c8b191ce81c4bba008b4a07ef3b4b96817f
AsepSahrudin18 <asepsahrudin0399@gmail.com>
2023-12-25 01:43:43 +0700

    update profile customer

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use DB;
 
 class ProfileController extends Controller
 {
@@ -71,7 +72,57 @@ class ProfileController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        // dd($request->all());
+        // proses input data
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'role' => 'required|string|max:255',
+            'pekerjaan' => 'required|string|max:255',
+            'telp' => 'required|numeric',
+            // 'foto_user' => 'nullable|image|mimes:jpg,jpeg,png,gif,svg|max:2048',
+        ]);
+
+        //------------foto lama apabila admin ingin ganti foto kamar-----------
+        // $foto = DB::table('users')->select('foto_user')->where('id',$id)->get();
+        // foreach($foto as $f){
+        //     $namaFileFotoLama = $f->foto_user;
+        // }
+
+        /**
+         * Jika user ingin ganti foto profile
+         * Jika ada foto lama ada, maka hapus foto lama terlebih dahulu danganti foto baru
+         */
+        // if(!empty($request->foto_user)){
+        //     if(!empty($kost_id->foto_user)) unlink('admin/img/'.$kost_id->foto_user);
+        //     $fileName = 'foto_user-'.$request->name.'.'.$request->foto_user->extension();
+        //     //$fileName = $request->foto->getClientOriginalName();
+        //     $request->foto_user->move(public_path('admin/img/users/'),$fileName);
+        // }
+        // /**
+        //  * Jika admin tidak update foto kamar maka, pakai foto lama
+        //  */
+        // else{
+        //     $fileName = $namaFileFotoLama;
+        // }
+
+        /**
+         * Lakukan update data dari request form edit
+         */
+        DB::table('users')->where('id',$id)->update(
+            [
+                'name' => $request->name,
+                'email' => $request->email,
+                'role' => $request->role,
+                'pekerjaan' => $request->pekerjaan,
+                'telp' => $request->telp,
+                // 'foto_user' => $fileName,
+                // 'password' => Hash::make($request->password),
+                'updated_at'=>now(),
+            ]);
+
+        return redirect()->route('customer-profile.index')->with('success','Data user berhasil di Update!');
+
     }
 
     /**
